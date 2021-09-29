4c0ced52d78d8d992dbaeff231268a440e7f7068
Frank Hu <44072346+YokaiMain@users.noreply.github.com>
2021-09-29 11:39:21 +0200

    hex mapping

--- index.js
+++ index.js
@@ -34,9 +34,13 @@ console.log(br.toBraille("B")); //Some limits using this library not capitals
 
 
 //TODO
-//Create map that maps our braille output to 2 rotation angles.
-//Potential i.e. format "braille" -> 090180 = left rot 90 and right rot 180
-//other format is binary i.e. "braille"-> 010111 first 3 digit form left rot, other form right rot = ⠺
+//Some byte magic blabla:
+//UTF-8 encoding for braille is as follow: its an 2X8 cell and each 1 and 0 represent dot on/off + 2800 offset
+//What we need to do is take the hexdec value - 2800 and then to binary we get an 8 bit data of our braille
+//https://en.wikipedia.org/wiki/Braille_Patterns for exactly how they mapped it
+// either way first 2 bits can be ommited (they rep dot 7 and 8),but since its easier to send bytes then bit we'll just keep it.
+//Then its just reading the byte per bit from right to left to know which dot is on.
+//for example 01101011 is the same as dots 124
 const hex_rot = new Map();
 
 
