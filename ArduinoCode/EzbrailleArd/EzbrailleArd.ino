8494d63bf52fb3d9464347b99f20845043712bcf
Frank Hu <44072346+YokaiMain@users.noreply.github.com>
2021-10-06 21:15:00 +0200

    Arduino code

---
+++ ArduinoCode/EzbrailleArd/EzbrailleArd.ino
@@ -0,0 +1,37 @@
+
+#include<Servo.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Servo Myservo;
+  Myservo.attach(3);
+  Serial.print(78);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+//just define the switch case here don't bother about the rest
+//note each case must be with with single quotation mark. i.e. 'k' not "k", double quotation will be treated as a string.
+int Compare(char x){
+  int result;
+  switch (x) {
+    case ' ':
+      // this is space mapped to the empty cell
+      break;
+    case '!':
+      // this is the translation '!' -> '⠁'
+      break;
+    case character:
+      //in comments what mapped to what. you can just copy paste the braille in the comments like this ⠽⠕⠥⠀⠙⠊⠙⠀⠇⠑
+      //complete the cases
+      break;
+    default:
+      // default case is if everything above fails
+      break;
+  }
+  return result;
+ }
