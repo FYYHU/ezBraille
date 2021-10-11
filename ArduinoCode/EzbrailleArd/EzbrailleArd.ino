991623631faa719d9eaab94d8f0e37f21e15f90d
Fantastabel <91530093+Fantastabel@users.noreply.github.com>
2021-10-11 17:05:02 +0200

    Update EzbrailleArd.ino

--- ArduinoCode/EzbrailleArd/EzbrailleArd.ino
+++ ArduinoCode/EzbrailleArd/EzbrailleArd.ino
@@ -24,13 +24,192 @@ int Compare(char x){
       break;
     case '!':
       //'!' -> '⠁'
-      // this is the translation '!' -> '⠁'
       break;
     case '"':
-      //'"' -> ''
+      //'"' -> '⠂'
       break;
     case '#':
-      //'#' -> ''
+      //'#' -> '⠃' 
+      break
+    case'$': 
+      //'$' -> '⠄'
+      break;
+    case'%':
+      //'%' -> '⠅'
+      break;
+    case'&':
+      //'&' ->'⠆'
+      break;
+    case''':
+      //''' ->'⠇'
+      break;
+    case'(':
+      //'(' ->'⠈'
+      break;
+    case')':
+      //')' ->'⠉'
+      break;
+    case'*':
+      //'*' ->'⠊'
+      break;
+    case'+':
+      //'+' ->'⠋'
+      break;
+    case',':
+      //',' ->'⠌'
+      break;
+    case'-':
+      //'-' ->'⠍'
+      break;
+    case'.':
+      //'.' ->'⠎'
+      break;
+    case'/':
+      //'/' ->'⠏'
+      break;
+    case'0':
+      //'0' ->'⠐'
+      break;
+    case'1':
+      //'1' ->'⠑'
+      break;
+    case'2':
+      //'2' ->'⠒'
+      break;
+    case'3':
+      //'3' ->'⠓'
+      break;
+    case'4':
+      //'4' ->'⠔'
+      break;
+    case'5':
+      //'5' ->'⠕'
+      break;
+    case'6':
+      //'6' ->'⠖'
+      break;
+    case'7':
+      //'7' ->'⠗'
+      break;
+    case'8':
+      //'8' ->'⠘'
+      break;
+    case'9':
+      //'9' ->'⠙'
+      break;
+    case':':
+      //':' ->'⠚'
+      break;
+    case';':
+      //';' ->'⠛'
+      break;
+    case'<':
+      //'<' ->'⠜'
+      break;
+    case'=':
+      //'=' ->'⠝'
+      break;
+    case'>':
+      //'>' ->'⠞'
+      break;
+    case'?':
+      //'?' ->'⠟'
+      break;
+    case'@':
+      //'@' ->'⠠'
+      break;
+    case'A':
+      //'A' ->'⠁'
+      break;
+    case'B':
+      //'B' ->'⠃'
+      break;
+    case'C':
+      //'C' ->'⠉'
+      break;
+    case'D':
+      //'D' ->'⠙'
+      break;
+    case'E':
+      //'E' ->'⠑'
+      break;
+    case'F':
+      //'F' ->'⠋'
+      break;
+    case'G':
+      //'G' ->'⠛'
+      break;
+    case'H':
+      //'H' ->'⠓'
+      break;
+    case'I':
+      //'I' ->'⠊'
+      break;
+    case'J':
+      //'J' ->'⠚'
+      break;
+    case'K':
+      //'K' ->''
+      break;
+    case'L':
+      //'L' ->''
+      break;
+    case'M':
+      //'M' ->''
+      break;
+    case'N':
+      //'N' ->''
+      break;
+    case'O':
+      //'O' ->''
+      break;
+    case'P':
+      //'P' ->''
+      break;
+    case'Q':
+      //'Q' ->''
+      break;
+    case'R':
+      //'R' ->''
+      break;
+    case'S':
+      //'S' ->''
+      break;
+    case'T':
+      //'T' ->''
+      break;
+    case'U':
+      //'U' ->''
+      break;
+    case'V':
+      //'V' ->''
+      break;
+    case'W':
+      //'W' ->''
+      break;
+    case'X':
+      //'X' ->''
+      break;
+    case'Y':
+      //'Y' ->''
+      break;
+    case'Z':
+      //'Z' ->''
+      break;
+    case']':
+      //']' ->''
+      break;
+    case'[':
+      //'[' ->''
+      break;
+    case'/':
+      //'/' ->''
+      break;
+    case'^':
+      //'^' ->''
+      break;
+    case'_':
+      //'_' ->''
       break;
     default:
       // default case is if everything above fails
