2a9fe82ebcac1176f2e40d40ab55b25172b1f49a
Frank Hu <44072346+YokaiMain@users.noreply.github.com>
2021-10-12 20:19:05 +0200

    Update next current previous array

--- ArduinoCode/EzbrailleArd/SDFiles/SDFiles.ino
+++ ArduinoCode/EzbrailleArd/SDFiles/SDFiles.ino
@@ -33,19 +33,31 @@ char prev[6];
 char current[6];
 char next[6];
 //class has to be created before object is being created
+//FileSyst class manages the files
  class FileSyst {
     private:
-      File root = SD.open("/");
+      int counter_files = 0; //use counter when toggling
+      int Number_files = 3;
+      File Reading;
     protected:
+      File root = SD.open("/");
       String SDFile1;
       String SDFile2;
       String SDFile3;
     public:
+      //6 character cache
+      //init with empty array
+      char Prev_Line[6]= {' ',' ',' ',' ',' ',' '};
+      char Current_Line[6]= {' ',' ',' ',' ',' ',' '};
+      char Next_Line[6]= {' ',' ',' ',' ',' ',' '};
+      char temp[6] = {' ',' ',' ',' ',' ',' '}; //temporary array used in calc
+
+      //File track;
       String Current_File;
-      
       String File_List[3];
       FileSyst() {//constructor
-        for (int i = 0; i < 4; i++) {//loop over the 4 first files (we skip file 1) and store the 3 file names
+      //loop over the 4 first files (we skip file 1) and store the 3 file names
+        for (int i = 0; i < Number_files + 1; i++) {
           File entry =  root.openNextFile();
           if (! entry) {
             // no more files
@@ -71,19 +83,61 @@ char next[6];
             SDFile3 = entry.name();
           }
         }
-        String File_List[] = {SDFile1,SDFile2,SDFile3};
+        String File_List[] = {SDFile1,SDFile2,SDFile3};//list first 3 files
+        //open the first file
+        Reading = SD.open(SDFile1);
       }
 
       //TODO create method to toggle Files
       void Toggle_File(){
-
+        //close current open file
+        Reading.close();
+        //assign new file to current
+        counter_files = (counter_files + 1)% Number_files;
+        Current_File = File_List[counter_files];
+        //reopen
+        Reading = SD.open(Current_File);
       }
       //TODO create method to Read file and return char
+      //Read char from file assumes file is open
       char Read_current(){
-
+        if (Reading.available()) {
+            return Reading.read();
+        }else{
+          //done reading
+          return ' ';
+        }
       }
-
-
+      //put next 6 characters in temp
+      void Read_next_temp_six_char(){
+        for (int i = 0; i < 6;i++){
+          temp[i] = Read_current();
+          //reassign each character
+          Prev_Line[i] = Current_Line[i];
+          Current_Line[i] = Next_Line[i];
+          Next_Line[i] = temp[i];
+        }
+        
+        
+      }
+      //previous next 6 characters in temp
+      void Read_prev_temp_six_char(){
+        unsigned long location = Reading.position()-12;
+        boolean checker = Reading.seek(location);
+          if (checker){
+            for (int i = 0; i < 6;i++){
+            temp[i] = Read_current();
+            Next_Line[i] = Current_Line[i];
+            Current_Line[i] = Prev_Line[i];
+            Prev_Line[i] = temp[i];
+            }
+          }
+          else {
+            for (int i = 0; i < 6;  i++){
+              temp[i] = ' ';
+              }
+            }
+      }
 
   };
 
@@ -106,7 +160,7 @@ void setup() {
 
   Serial.println("done!");
 
-  FileSyst k;
+  FileSyst FileSystemSD;
 }
 
 void loop() {
