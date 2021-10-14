14bcd298df0b7d3b5c1cf68ef7cc69b11c9ea68c
Frank Hu <44072346+YokaiMain@users.noreply.github.com>
2021-10-14 11:55:01 +0200

    comments

--- ArduinoCode/EzbrailleArd/SDFilesV2/SDFilesV2.ino
+++ ArduinoCode/EzbrailleArd/SDFilesV2/SDFilesV2.ino
@@ -1,8 +1,5 @@
 /*
-  Listfiles
 
-  This example shows how print out the files in a
-  directory on a SD card
 
   The circuit:
    SD card attached to SPI bus as follows:
@@ -10,15 +7,6 @@
  ** MISO - pin 12
  ** CLK - pin 13
  ** CS - pin 4 (for MKRZero SD: SDCARD_SS_PIN)
-
-  created   Nov 2010
-  by David A. Mellis
-  modified 9 Apr 2012
-  by Tom Igoe
-  modified 2 Feb 2014
-  by Scott Fitzgerald
-
-  This example code is in the public domain.
 
 */
 #include <SPI.h>
