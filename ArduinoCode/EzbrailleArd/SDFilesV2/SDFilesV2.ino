876fabf4b5321b000c8eaa224065cb6fca57e3a8
Frank Hu <44072346+YokaiMain@users.noreply.github.com>
2021-10-19 11:46:07 +0200

    change comments

--- ArduinoCode/EzbrailleArd/SDFilesV2/SDFilesV2.ino
+++ ArduinoCode/EzbrailleArd/SDFilesV2/SDFilesV2.ino
@@ -3,10 +3,10 @@
 
   The circuit:
    SD card attached to SPI bus as follows:
- ** MOSI - pin 11
- ** MISO - pin 12
- ** CLK - pin 13
- ** CS - pin 4 (for MKRZero SD: SDCARD_SS_PIN)
+ ** MOSI - pin 51
+ ** MISO - pin 50
+ ** CLK - pin 52
+ ** CS - pin 53 (for MKRZero SD: SDCARD_SS_PIN)
 
 */
 #include <SPI.h>
